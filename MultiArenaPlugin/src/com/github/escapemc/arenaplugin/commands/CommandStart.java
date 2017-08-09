0b788caa4f43e3c28851667d1edef070aceb32ed
Rob <crazy4pugs37@gmail.com>
2017-08-08 20:07:10 -0400

    1.0.0

---
+++ MultiArenaPlugin/src/com/github/escapemc/arenaplugin/commands/CommandStart.java
@@ -0,0 +1,17 @@
+package com.github.escapemc.arenaplugin.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandStart implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		
+		
+		return false;
+	}
+
+}
